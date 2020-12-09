808bf6e525205ed092de226b10c7b23c9f14f100
Secundino Martinez <Dino.Martinez.514@gmail.com>
2020-12-08 23:13:44 -0800

    added a death counter to prove I can use state in react components

--- src/Footer.js
+++ src/Footer.js
@@ -1,8 +1,22 @@
-import './Footer.css'
+import './Footer.css';
+import React, { useState } from 'react';
+
 function Footer() {
+  const [count, setCount] = useState(0);
   return (
     <div className='Footer'>
       Copyright 2020
+
+      <div>
+        <button onClick={() => setCount(count - 1)}>
+        -1 Death
+        </button>
+        <p>{count} deaths and counting...</p>
+        <button onClick={() => setCount(count + 1)}>
+        +1 Death
+        </button>
+      </div>
+      <input type='number' onChange={(e) => setCount(e.target.value)} value={count}/>
     </div>
   );
 }
