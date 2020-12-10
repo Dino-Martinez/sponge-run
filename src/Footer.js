7f7855ab3790bf4bb393ba25f9bf91de05e5e818
Secundino Martinez <Dino.Martinez.514@gmail.com>
2020-12-10 14:46:52 -0800

    removed death counter because it is unnecesary

--- src/Footer.js
+++ src/Footer.js
@@ -1,27 +1,11 @@
 import './Footer.css';
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 function Footer() {
-  const [count, setCount] = useState(0);
   return (
     <div className="Footer">
       Copyright 2020
-
-      <div>
-        <button type="button" onClick={() => setCount(count - 1)}>
-          -1 Death
-        </button>
-        <p>
-          {count}
-          {' '}
-          deaths and counting...
-        </p>
-        <button type="button" onClick={() => setCount(count + 1)}>
-          +1 Death
-        </button>
-      </div>
-      <input type="number" onChange={(e) => setCount(e.target.value)} value={count} />
       <Link className="Link" to="/leaderboard/a">Check out the top runs!</Link>
     </div>
   );
